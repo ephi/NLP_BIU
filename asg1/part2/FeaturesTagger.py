e2260bdf3a6cfd09bfdf0e232060f10b6745f2fc
Ella475 <Ellashalom1995@gmail.com>
2022-03-31 18:42:05 +0300

    my files

--- asg1/part2/FeaturesTagger.py
+++ asg1/part2/FeaturesTagger.py
@@ -1,108 +1,56 @@
-import sys
-import re
 import pickle
-import numpy as np
-from joblib import dump, load
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.linear_model import LogisticRegression
+import sys
+import time
 from ExtractFeatures import extract
-import utils
+from part1.GreedyTag import report_accuracy
+
+g_word_dict = {}
 
 
-def load_model(mdl_f_name):
-    clf = load(mdl_f_name)
-    return clf
+def predict_sentence(model, sentence, d):
+    sentence += ['endline', 'endline']
+    prev_pair = ('startline', '<S>')
+    prev_prev_pair = ('startline', '<S>')
+    predictions = []
+
+    for i, word in enumerate(sentence[:-2]):
+        word_feature = extract(word, [prev_pair, prev_prev_pair], sentence[i:(i+2)], g_word_dict)
+        feature_vector = d.transform(word_feature)
+        prediction = model.predict(feature_vector)
+        predictions.append(prediction)
+
+        prev_prev_pair = prev_pair
+        prev_pair = (word, prediction)
+
+    return predictions
 
 
-def get_most_freq_tag(tags):
-    cntrs = {}
-    for tag in tags:
-        cnt = cntrs.get(tag, 0)
-        cntrs[tag] = cnt + 1
-    mx_v = 0
-    mx_k = tags[0]
-    for k, v in cntrs.items():
-        if v > mx_v:
-            mx_k = k
-            mx_v = v
-    return mx_k
+if __name__ == "__main__":
+    input_file_name = sys.argv[1]
+    model_file_name = sys.argv[2]
+    feature_map_file = sys.argv[3]
+    output_file = sys.argv[4]
 
+    start = time.perf_counter()
 
-def load_model_input(input_f_name, feat_map_f_name):
-    # load additional information from feature map file
-    with open(feat_map_f_name, 'rb') as file:
-        data_dic = pickle.load(file)
-        v = data_dic["v"]
-        known_word_tags = data_dic["known_word_tags"]
-        tag_index_dict = data_dic["tag_index_dict"]
+    with open(input_file_name, "r", encoding="utf-8") as input_file:
+        lines = input_file.readlines()
 
-    word_tag_pair_list = []
-    with open(input_f_name, 'r', encoding='utf-8') as file:
-        for line in file.readlines():
-            start_pair = ("startline", "<S>")
-            word_tag_pair_list.append(start_pair)
-            word_tag_pair_list.append(start_pair)
-            for s in line.strip().split(" "):
-                word = s
-                tags = known_word_tags.get(word, None)
-                if tags is None:
-                    tags = known_word_tags.get(word.lower(), None)
-                    if tags is None:
-                        tags = known_word_tags[utils.create_word_signature(word)]
-                pair = (word, get_most_freq_tag(tags))
-                word_tag_pair_list.append(pair)
-            end_pair = ("endline", "<E>")
-            word_tag_pair_list.append(end_pair)
-            word_tag_pair_list.append(end_pair)
-    X = []
-    for i, word_tag_pair in enumerate(word_tag_pair_list):
-        if word_tag_pair[0] == "startline" or word_tag_pair[0] == "endline":
-            continue
-        features = extract(i, word_tag_pair_list)
-        del features["w_not_feature"]
-        X.append(features)
-    X = v.transform(X)
-    return X, word_tag_pair_list, tag_index_dict
+    sentences = [line.strip().split(' ') for line in lines]
 
+    with open(feature_map_file, 'rb') as file:
+        v = pickle.load(file)
+        g_word_dict = pickle.load(file)
 
-def write_prediction_output(pred_f_name, word_list, y_hat):
-    with open(pred_f_name, 'w', encoding='utf-8') as file:
-        str_res = ""
-        y_hat_runner = 0
-        for i, pair in enumerate(word_list):
-            if pair[0] == "endline":
-                continue
-            if pair[0] == "startline":
-                if i > 0 and word_list[i - 1][0] == "endline":
-                    str_res = str_res.rstrip()
-                    str_res += "\n"
-                    file.write(str_res)
-                    str_res = ""
-                continue
-            pred = y_hat[y_hat_runner]
-            str_res += pair[0] + "/" + pred + " "
-            y_hat_runner += 1
-        str_res = str_res.rstrip()
-        file.write(str_res)
+    model = pickle.load(open(model_file_name, 'rb'))
 
+    str_i = ''
+    for sentence in sentences:
+        predictions = predict_sentence(model, sentence)
+        str_i += ' '. join([f'{word}/{tag}' for i, (word, tag) in zip(sentences, predictions)]) + '\n'
 
-if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print("invalid args, expected: input_file_name model_file_name feature_map_file output_file exiting...")
-        exit(1)
-    input_f_name = sys.argv[1]
-    mdl_f_name = sys.argv[2]
-    feat_map_f_name = sys.argv[3]
-    pred_f_name = sys.argv[4]
-    X, word_list, tag_index_dict = load_model_input(input_f_name, feat_map_f_name)
-    classifier = load_model(mdl_f_name)
+    with open(output_file, "w") as out_file:
+        out_file.write(str_i)
 
-    # predict y_hat & prepared it as tag list
-    y_hat = classifier.predict_log_proba(X)
-    y_hat = np.argmax(y_hat, axis=1)
-    y_hat_as_tags = []
-    # invert the tag_index_dict
-    tag_index_dict = {v: k for k, v in tag_index_dict.items()}
-    for p in y_hat:
-        y_hat_as_tags.append(tag_index_dict[p])
-    write_prediction_output(pred_f_name, word_list, y_hat_as_tags)
+    report_accuracy({'dev_set': '../data/ass1-tagger-dev', 'pred_file': output_file})
+    print("Elapsed time %.2f [m]" % ((time.perf_counter() - start) / 60))
--- asg1/part2/TrainModel.py
+++ asg1/part2/TrainModel.py
@@ -1,91 +1,55 @@
 import sys
-import re
-from joblib import dump, load
 import pickle
+import time
+
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.linear_model import SGDClassifier
-import re
-import utils
-import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+g_word_set = set()
 
 
-def map_features_to_vectors(features_f_name, feature_map_file_name):
-    tag_index_dict = {}
-    known_word_tags = {}
-    y = []
-    X = []
-    tag_index = 0
-    with open(features_f_name, 'r', encoding='utf-8') as file:
-        for line in file.readlines():
-            r = line.strip().split(" ")
-            # skip blank lines
-            if len(r) == 0:
-                continue
-            # map feature label to unique key
-            found_tag_index = tag_index_dict.get(r[0], -1)
-            nxt_tag_index = tag_index
-            if found_tag_index == -1:
-                found_tag_index = tag_index
-                tag_index_dict[r[0]] = tag_index
-                nxt_tag_index += 1
-            y.append(found_tag_index)
-            tag_index = nxt_tag_index
-            # add dict reprsenting the feature
-            feature_dict = {}
-            discreate_feature = r[1:]
-            for x in discreate_feature:
-                x_key, x_val = x.split("~=")
-                if x_key == "w_not_feature":
-                    keys = [x_val, x_val.lower(), utils.create_word_signature(x_val)]
-                    for key in keys:
-                        tags = known_word_tags.get(key, [])
-                        tags.append(r[0])
-                        known_word_tags[key] = tags
-                else:
-                    try:
-                        feature_dict[x_key] = int(x_val)
-                    except:
-                        feature_dict[x_key] = x_val
-            X.append(feature_dict)
-    v = DictVectorizer(sparse=True)
-    v = v.fit(X)
-    # save the mapping X into feature_map_file
-    # save the known tags dic into feature_map_file
-    with open(feature_map_file_name, 'wb') as outfile:
-        data_to_dump = {"v": v, "tag_index_dict": tag_index_dict, "known_word_tags": known_word_tags}
-        pickle.dump(data_to_dump, outfile)
-    X = v.transform(X)
+def load_features(features_f_name):
+    with open(features_f_name, "r", encoding="utf-8") as featuresFile:
+        lines = featuresFile.readlines()
 
-    return X, y
+    words_features = []
+    labels = []
+    for i, line in enumerate(lines):
+        features, label = line.strip().rsplit(' ', 1)
+        features = features.split(' ')
+        pairs = [tuple(pair.rsplit('=', 1)) for pair in features]
+        features_dict = dict(pairs)
+        g_word_set.add(features_dict.get('w', ''))
+        words_features.append(features_dict)
+        labels.append(label)
+
+    return words_features, labels
 
 
-def train_model(X, y):
-    classifier = SGDClassifier(loss="log")
-    y = np.array(y)
-    for i in range(0, 30):
-        idx = np.random.randint(X.shape[0], size=int(X.shape[0] * 0.2))
-        Y_batch = y[idx]
-        X_batch = X[idx, :]
-        cls_cnt = None
-        if i == 0:
-            cls_cnt = np.unique(y)
-        classifier = classifier.partial_fit(X_batch, Y_batch, classes=cls_cnt)
-    return classifier
+def to_vectors(features):
+    d = DictVectorizer()
+    feature_vectors = d.fit_transform(features)
+    with open('feature_map_file', 'wb') as file:
+        pickle.dump(d, file)
+        pickle.dump(g_word_set, file)
+
+    return feature_vectors
 
 
-def save_model(clf, model_f_name):
-    dump(clf, model_f_name)
+def train(features_f_name, model_f_name):
+    features, labels = load_features(features_f_name)
+    feature_vectors = to_vectors(features)
+    logistic_reg = LogisticRegression(random_state=0, max_iter=1000, multi_class='multinomial')
+    logistic_reg.fit(feature_vectors, labels)
+    pickle.dump(logistic_reg, open(model_f_name, 'wb'))
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("invalid args, expected: features_file model_file (optional: feature_map_file) exiting...")
+        print("invalid args, expected: features_f_name model_f_name exiting...")
         exit(1)
     features_f_name = sys.argv[1]
     model_f_name = sys.argv[2]
-    feature_map_file_name = "feature_map_file"
-    if len(sys.argv) > 3:
-        feature_map_file_name = sys.argv[3]
-    X, y = map_features_to_vectors(features_f_name, feature_map_file_name)
-    clf = train_model(X, y)
-    save_model(clf, model_f_name)
+    start = time.perf_counter()
+    train(features_f_name, model_f_name)
+    print("Elapsed time: %.2f [m]" % ((time.perf_counter() - start) / 60))
